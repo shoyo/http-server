7a55fa0cdf198330e2f30541ce2e9500c449e5b4
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-10-03 20:54:38 +0900

    Implement workers and threadpool, enable multithreading

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,4 @@
+use custom_web_server::ThreadPool;
 use std::io::prelude::*;
 use std::fs;
 use std::net::TcpListener;
@@ -5,11 +6,14 @@ use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -18,13 +22,14 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-
     let response;
+
     if buffer.starts_with(get) {
         response = construct_response("200 OK", "templates/hello.html");
     } else {
         response = construct_response("404 NOT FOUND", "templates/404.html");
     }
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
